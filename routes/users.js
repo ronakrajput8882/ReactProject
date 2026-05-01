72ecf22ccba46202d59637f363453ba162667338
Ronaksinh Rajput <120843540+ronakrajput8882@users.noreply.github.com>
2026-05-01 21:03:44 +0530

    Remove unnecessary whitespace in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,7 @@
 const express = require("express");
 const router = express.Router();
-
-// ✅ Correct import of handler functions
 const { getUserByEmail, createUser } = require("../controllers/userController");
 
-// ✅ Define routes using functions
 router.post("/", createUser);
 router.get("/:email", getUserByEmail);
 
